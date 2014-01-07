fb4851ef11e6d233118f1d6f0ac013beae7fb825
Apprentice <twwaneka@gmail.com>
2014-01-06 18:09:49 -0800

    refactored to jQuery

--- client.js
+++ client.js
@@ -3,9 +3,7 @@ function Game(){
 
 Game.prototype = {
   init: function(){
-    var field = document.getElementById('field')
-    field.addEventListener('keypress',this.isCorrect,false)
-    console.log('derp')
+    $('#field').keypress(this.isCorrect)
   },
 
   isCorrect: function(){
@@ -16,7 +14,7 @@ Game.prototype = {
 
 function pageLoad(){
   var game = new Game
-  window.addEventListener('load',function(){
+  $(document).ready(function(){
     game.init()})
   var socket = io.connect(window.location.origin)
 }
